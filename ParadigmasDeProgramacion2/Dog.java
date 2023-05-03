00b083d2bbffa133a5bedf3f8a7f2c8ff8423b28
UrielHdz1 <127629606+UrielHdz1@users.noreply.github.com>
2023-05-03 07:33:53 -0600

    Add files via upload

---
+++ ParadigmasDeProgramacion2/Dog.java
@@ -0,0 +1,13 @@
+package com.pp2;
+
+public class Dog implements Animal{
+	public void move() {
+		// TODO Auto-generated method stub
+		System.out.println("The dog is moving...");
+	}
+	public void eat() {
+		// TODO Auto-generated method stub
+		System.out.println("The dog is eating...");
+	}
+	
+}
